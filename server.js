06a3342aca65d5bd7f0542052b40a4cbfc2b42bc
Chris Hundhausen <hundhaus@wsu.edu>
2020-10-15 14:41:04 -0700

    Trying to switch back to ES6 syntax in server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // server.js -- A simple Express.js web server for serving a React.js app
-//import path from 'path';
-//import express from 'express';
-path = require('path');
-express = require('express');
+import path from 'path';
+import express from 'express';
+//path = require('path');
+//express = require('express');
 
 const PORT = process.env.HTTP_PORT || 8081;
 const app = express();
