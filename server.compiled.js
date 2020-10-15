441bbe29ad2dea53616d9f694a808e78f2ba0c50
Chris Hundhausen <hundhaus@wsu.edu>
2020-10-15 15:46:31 -0700

    Updated server.compiled

--- server.compiled.js
+++ server.compiled.js
@@ -1,13 +1,17 @@
 "use strict";
 
+var _path = _interopRequireDefault(require("path"));
+
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
 // server.js -- A simple Express.js web server for serving a React.js app
-//import path from 'path';
-//import express from 'express';
-path = require('path');
-express = require('express');
+//path = require('path');
+//express = require('express');
 var PORT = process.env.HTTP_PORT || 8081;
-var app = express();
-app.use(express["static"](path.join(__dirname, 'client', 'build')));
+var app = (0, _express["default"])();
+app.use(_express["default"]["static"](_path["default"].join(__dirname, 'client', 'build')));
 app.listen(PORT, function () {
   console.log("Server listening at port ".concat(PORT, "."));
 });
