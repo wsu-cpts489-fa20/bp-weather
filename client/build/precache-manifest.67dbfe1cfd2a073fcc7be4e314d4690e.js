fe3584a891e5050d88092eea789c53560a7cf8e4
Chris Hundhausen <hundhaus@wsu.edu>
2020-10-15 09:24:39 -0700

    Added build/ directory previously ignored!

---
+++ client/build/precache-manifest.67dbfe1cfd2a073fcc7be4e314d4690e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0565a51182ac9794bf38b4252a3fea7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fc6993ba99c1d291dfe",
+    "url": "/static/css/2.1769b337.chunk.css"
+  },
+  {
+    "revision": "8b3821bac57a640fec5d",
+    "url": "/static/css/main.26c7c6c4.chunk.css"
+  },
+  {
+    "revision": "5fc6993ba99c1d291dfe",
+    "url": "/static/js/2.4fa568d5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.4fa568d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b3821bac57a640fec5d",
+    "url": "/static/js/main.ee38e106.chunk.js"
+  },
+  {
+    "revision": "19793d9fa96413d1c44d",
+    "url": "/static/js/runtime-main.0d575dc6.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
