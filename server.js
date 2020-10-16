3f04746d05ac06569a593d6f60f7963869728b7e
Chris Hundhausen <hundhaus@wsu.edu>
2020-10-15 22:09:27 -0700

    Revert "Updated package.json" This reverts commit da79ea3f7d074448572b57dfb650b8f2f3d9b00f. Reverting because changes broke eb deploy

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // server.js -- A simple Express.js web server for serving a React.js app
 import path from 'path';
 import express from 'express';
+//path = require('path');
+//express = require('express');
+
 const PORT = process.env.HTTP_PORT || 8081;
 const app = express();
 app.use(express.static(path.join(__dirname, 'client', 'build')));
