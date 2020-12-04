ba2997c6d871c9754e52b9f374b3ba5828c23919
Jimmy Zheng <37029188+ByteSizeError@users.noreply.github.com>
2020-12-03 22:09:51 -0800

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import path from 'path';
 import express from 'express';
 require('dotenv').config();
 
-const LOCAL_PORT = 8081;
+const LOCAL_PORT = 8080;
 const DEPLOY_URL = "http://localhost:8081";
 const PORT = process.env.HTTP_PORT || LOCAL_PORT;
 const GithubStrategy = passportGithub.Strategy;
